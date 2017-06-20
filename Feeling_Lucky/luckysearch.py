71c0942c9244f9c0300b0e488cf81cd2795c990f
barpit20 <arpit16229@iiitd.ac.in>
2017-06-20 15:40:00 +0530

    Add LuckySearch

---
+++ Feeling_Lucky/luckysearch.py
@@ -0,0 +1,9 @@
+import requests, sys, webbrowser, bs4
+print('Googling...')
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text)
+linkElems = soup.select('.r a')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
